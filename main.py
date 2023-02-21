6d1e5c84fa86bf3b82b14c0a9f134beb7a5cc159
Juli Tay <julitays@icloud.com>
2023-02-21 18:22:56 +0300

    feat: added gamecode which need help

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-                                                        # Проверка знаний ООП и создание карточной игры: "Пьяница"
+                                            # Проверка знаний ООП и создание карточной игры: "Пьяница"
 from random import shuffle
 class Card:
     suits = ['пикей', 'бубей', 'червей', 'крестей']     # Масти, которые могут быть у выпавших карт
@@ -64,3 +64,55 @@ class Player:
         self.name = name
         self.wins = 0                           # для отслеживания количества раундов, выигранных игроком
         self.card = None                        # для представления карты, которую в данный момент держит игрок;
+
+class Game:
+    def __init__(self):
+        name1 = input('Имя игрока 1: ')
+        name2 = input('Имя игрока 2: ')
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
+
+    def wins(self, winner):
+        w = '{} забирает карты'
+        w = w.format(winner)
+        print(w)
+
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} кладет {}, а {} кладет {}"
+        d = d.format(p1n, p1c, p2n, p2c)
+        print(d)
+
+    def play_game(self):
+        cards = self.deck.cards
+        print('Поехали!')
+        while len(cards) >= 2:
+            m = "Нажмите X для выхода. Нажмите любую другую клавишу для начала игры."
+            response = input(m)
+            if response == 'X':
+                break
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n, p1c, p2n, p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+
+        win = self.winner(self.p1, self.p2)
+        print('Игра окончена! {} выиграл!'.format(win))
+
+        def winner(self, p1, p2):
+            if p1.wins > p2.wins:
+                return p1.name
+            if p1.wins < p2.wins:
+                return p2.name
+            return "Ничья!"
+
+game = Game()
+game.play_game()
